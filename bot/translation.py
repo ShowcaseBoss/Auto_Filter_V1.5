647644ce349ea27dbcc31a310b3a61e0f231d57e
ShowcaseBoss <91422286+ShowcaseBoss@users.noreply.github.com>
2021-09-30 13:32:40 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,13 +7,15 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Movie & Series Filters for sale details in helptab
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+👇🏻👇👇🏿For Feedback & Help Contact👇🏿👇👇🏻
+@ShowcasE_Feedback
 </i>"""    
     
     HELP_TEXT = """
+<b><i><u>For filters contact</u></i></b>@ShowcasE_Feedback
+
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
@@ -42,17 +44,14 @@ For More Details Click Help Button Below..
             -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@CrazyBotsz
+           
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/ShowcasE_Feedback">Showcase Boss</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/ShowcaseBoss/Auto_Filter_V2">Click Me</a></i>
 """
